f6e0be0dedf1c7f812f38a5e4d49a6d51461f720
Arun Gangadharan <arun.ganga@gmail.com>
2020-06-15 11:01:17 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,3 +5,8 @@ cd git-checkout
 git clone https://github.com/arungangadharan/LockerApp.git
 cd LockerApp
 mvn clean install
+
+echo [InternetShortcut] >> "%USERPROFILE%\desktop\Lock.url"
+echo URL="C:\WINDOWS\NOTEPAD.EXE" >> "%USERPROFILE%\desktop\Lock.url"
+echo IconFile="D:\workbox\Lock.ico" >> "%USERPROFILE%\desktop\Lock.url"
+echo IconIndex=20 >> "%USERPROFILE%\desktop\Lock.url"
