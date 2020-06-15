ee87a05b908330f76978fb864f1ac467b6f82c44
Arun Gangadharan <arun.ganga@gmail.com>
2020-06-15 11:03:03 +0530

    Add files via upload

---
+++ create Icon.bat
@@ -0,0 +1,5 @@
+@echo off
+echo [InternetShortcut] >> "%USERPROFILE%\desktop\NOTEPAD.url"
+echo URL="C:\WINDOWS\NOTEPAD.EXE" >> "%USERPROFILE%\desktop\NOTEPAD.url"
+echo IconFile="D:\workbox\Lock.ico" >> "%USERPROFILE%\desktop\NOTEPAD.url"
+echo IconIndex=20 >> "%USERPROFILE%\desktop\NOTEPAD.url"
